954bd4dc8f786ae825d56036c1d834cc20595ba6
Brandon Hubbard <bkhubbard@gmail.com>
2019-01-19 14:43:29 -0800

    Uninstall File.

--- uninstall.php
+++ uninstall.php
@@ -7,3 +7,10 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+// Delete Our Options.
+delete_option( 'rest_api_cache' );
+
+// Delete All Cache.
+$rest_api_cache = new WP_REST_API_CACHE();
+$rest_api_cache->delete_all_cache();
