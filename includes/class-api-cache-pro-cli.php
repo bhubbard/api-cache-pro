4b6fbf7dce8ef78faf48b2283cdbc7cd92ad026a
Brandon Hubbard <bkhubbard@gmail.com>
2019-01-19 20:22:01 -0800

    cli command

--- includes/class-api-cache-pro-cli.php
+++ includes/class-api-cache-pro-cli.php
@@ -1,31 +1,60 @@
 <?php
+/**
+ * WP-CLI Support for API Cache Pro.
+ *
+ * @package api-cache-pro
+ */
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	exit;
-}
 
-if ( ! class_exists( 'API_Cache_Pro_CLI' ) ) {
-
-	/**
-	 * API_Cache_Pro_CLI class.
-	 */
-	class API_Cache_Pro_CLI {
+	if ( ! class_exists( 'API_Cache_Pro_CLI' ) ) {
 
 		/**
-		 * api_cache_pro_cli function.
+		 * API Cache Pro.
 		 *
-		 * @access public
-		 * @param mixed $args
-		 * @param mixed $assoc_args
-		 * @return void
+		 * ## OPTIONS
+		 *
+		 * delete: Delete All Cache created by API Cache Pro.
+		 *
+		 *
+		 * ## EXAMPLES
+		 *
+		 * wp api-cache-pro delete
 		 */
-		public function clear_cache() {
-		   WP_CLI::success( "The script has run!" );
-		};
+		class API_Cache_Pro_CLI {
 
+			/**
+			 * Constructor.
+			 *
+			 * @access public
+			 */
+			public function __construct() {
+
+			}
+
+			/**
+			 * Delete all Cache.
+			 *
+			 * @access public
+			 * @param mixed $args Arguments.
+			 * @param mixed $assoc_args Associated Arguments.
+			 */
+			public function delete( $args, $assoc_args ) {
+
+				$api_cache_pro = new API_CACHE_PRO();
+				$results       = $api_cache_pro->delete_all_cache();
+
+				if ( ! empty( $results ) ) {
+					WP_CLI::success( __( 'The Cache has been cleared.', 'api-cache-pro' ) );
+				} else {
+					WP_CLI::error( __( 'Cache is either empty, or there was an error.', 'api-cache-pro' ) );
+				}
+
+			}
+
+		}
+
+		WP_CLI::add_command( 'api-cache-pro', 'API_Cache_Pro_CLI' );
 
 	}
-
-	WP_CLI::add_command( 'api-cache-pro', 'API_Cache_Pro_CLI' );
-
 }
